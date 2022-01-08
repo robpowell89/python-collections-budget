1da438e9330b03d91502dfec6da5250418dacdb6
Robert Powell <90731739+robpowell89@users.noreply.github.com>
2022-01-07 23:04:36 -0500

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -37,7 +37,7 @@ expenses.read_expenses('data/spending_data.csv')
     globals=globals()
     ))
 
-    fix, ax = plt.subplots()
+    fig, ax = plt.subplots()
     labels = ['Necessary', 'Food', 'Unnecessary']
     divided_expenses_sum = []
     for category_exps in divided_set_comp:
